209c0b60aa0f69708e033573adbfbd1fc1bc247d
Simon Shapiro <simon.m.shapiro@gmail.com>
2013-07-10 15:37:59 +0100

    test.js dump

--- routes/test.js
+++ routes/test.js
@@ -1,4 +1,34 @@
-exports.index = function(req,res) {
-	dat={"hello":"world"}
-	res.render('test.jade',data={one:"hello world",json:JSON.stringify(dat)})
+DBROUTE='http://localhost:5984/'
+//DBROUTE='http://simons.iriscouch.com/'
+
+function couchBroker(doc) {
+	var docbase
+	this.docbase=doc
 }
+couchBroker.prototype = {
+	constructor: couchBroker,
+	wr:  function() {
+		console.log("Hello world "+this.docbase)
+	},
+	couchGET: function(guid,callBack) {
+		options={
+			url:DBROUTE+this.docbase+"/"+guid, //avoid hard coding the dbase
+			method:"GET"
+			}
+		console.log(JSON.stringify(options))
+		var request = require('request');
+		request(options, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				console.log("passing data back to browser"+JSON.stringify(body))
+//				res.render('editRule.jade',{data:{header:req.params.id,json:body}})
+		  }
+		})
+	}
+}
+
+c=new couchBroker("dictionary")
+c.wr()
+b=new couchBroker("B")
+b.wr()
+console.log(b.docbase+":"+c.docbase)
+c.couchGET("AT001","")
