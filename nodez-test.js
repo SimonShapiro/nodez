fe2f32fc7bf8af0ab7e50aeea94ee27bcd719ca5
Simon Shapiro <simon.m.shapiro@gmail.com>
2014-02-17 09:57:50 +0000

    Nodez-test pre state machine

---
+++ nodez-test.js
@@ -0,0 +1,42 @@
+var G = require('agentz/agentz.js')
+
+var _CB_deleteAll = function (error, response, body) {
+    console.log("back...")
+    if (!error && response.statusCode == 200) {
+        console.log("OK  "+response.statusCode+":"+options.url+":Neo4j results="+body)
+//        mac["start"]["end"]()
+    }
+    else {
+        console.log("!OK "+response.statusCode+":"+options.url+":Neo4j results="+body)
+    }
+//          callBack(response.statusCode,body)
+}
+deleteAll = function () {
+    var DBROUTE="http://localhost:7474/db/data/cypher/"
+    cypher={
+        "query":"match (m) optional match (m)-[r]->() delete m,r",
+        "params": {
+        }
+    }
+    console.log(JSON.stringify(cypher))
+    options={
+        url:DBROUTE,
+        method:"POST",
+        headers:{'content-type':'application/json'},
+        body:JSON.stringify(cypher)
+        }
+    console.log("Calling Neo4j broker with:"+JSON.stringify(options))
+    var request = require('request');
+    console.log('Http async')
+    request(options,_CB_deleteAll)
+}
+
+startTestSequence=function() {
+    deleteAll()
+}
+
+exports["Save a type"]=function(test) {
+    //    statemac(mac,"start","go")
+    startTestSequence()
+    test.done()
+}
