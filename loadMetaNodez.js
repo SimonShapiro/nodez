340367b41024cc246c3681b52d67dd25e9758d16
Simon Shapiro <simon.m.shapiro@gmail.com>
2014-02-27 07:46:51 +0000

    node-neo4j replaced with native rest api call

--- loadMetaNodez.js
+++ loadMetaNodez.js
@@ -2,13 +2,13 @@ var n=require("nodez/nodezv3.js")
 
 metaModel =[
     {"addMetaNode":{type:"Types",name:"Cluster",template:"{description:'string'}"}}
-    ,{"addMetaNode":{type:"Types",name:"Business Process",template:"{description:'string'}"}}
+    ,{"addMetaNode":{type:"Types",name:"BusinessProcess",template:"{description:'string'}"}}
     ,{"addMetaNode":{type:"Types",name:"Application",template:"{description:'string'}"}}
-    ,{"addLegalRelationship":{from:"Business Process",rel:"HAS_PARTS",to:"Business Process",template:"{}"}}    
+    ,{"addLegalRelationship":{from:"BusinessProcess",rel:"HAS_PARTS",to:"BusinessProcess",template:"{}"}}    
     ,{"addLegalRelationship":{from:"Cluster",rel:"HAS_PARTS",to:"Cluster",template:"{}"}}
-    ,{"addLegalRelationship":{from:"Business Process",rel:"RELIES_ON",to:"Application",template:"{}"}}    
-    ,{"addLegalRelationship":{from:"Business Process",rel:"NEXT",to:"Business Process",template:"{}"}}    
-    ,{"addLegalRelationship":{from:"Cluster",rel:"IS_ACCOUNTABLE_FOR",to:"Business Process",template:"{}"}}    
+    ,{"addLegalRelationship":{from:"BusinessProcess",rel:"RELIES_ON",to:"Application",template:"{}"}}    
+    ,{"addLegalRelationship":{from:"BusinessProcess",rel:"NEXT",to:"BusinessProcess",template:"{}"}}    
+    ,{"addLegalRelationship":{from:"Cluster",rel:"IS_ACCOUNTABLE_FOR",to:"BusinessProcess",template:"{}"}}    
     ,{"addLegalRelationship":{from:"Cluster",rel:"USES",to:"Application",template:"{}"}}    
 ]
 
--- loadNodez.js
+++ loadNodez.js
@@ -1,30 +1,30 @@
 var n=require("nodez/nodezv3.js")
 
 nodes=[
-    {"addTypedNode":{type:"Cluster",data:{name:"Treasury",decription:"",notes:"notes&comments"}}}
-    ,{"addTypedNode":{type:"Cluster",data:{name:"UK Retail Bank",decription:"",notes:"notes&comments"}}}
-    ,{"addTypedNode":{type:"Cluster",data:{name:"Investment Bank",decription:"",notes:"notes&comments"}}}
-    ,{"addTypedNode":{type:"Cluster",data:{name:"Western Europe",decription:"",notes:"notes&comments"}}}
-    ,{"addTypedNode":{type:"Cluster",data:{name:"Wealth",decription:"",notes:"notes&comments"}}}
-    ,{"addTypedNode":{type:"Cluster",data:{name:"Africa",decription:"",notes:"notes&comments"}}}
-    ,{"addTypedNode":{type:"Cluster",data:{name:"ALM",decription:"Asset and Liablility Management",notes:"notes&comments"}}}
+    {"addTypedNode":{type:"Cluster",data:{name:"Treasury",description:"",notes:"notes&comments"}}}
+    ,{"addTypedNode":{type:"Cluster",data:{name:"ALM",description:"Asset and Liablility Management",notes:"notes&comments"}}}
+    ,{"addTypedNode":{type:"Cluster",data:{name:"UK Retail Bank",description:"",notes:"notes&comments"}}}
+    ,{"addTypedNode":{type:"Cluster",data:{name:"Investment Bank",description:"",notes:"notes&comments"}}}
+    ,{"addTypedNode":{type:"Cluster",data:{name:"Western Europe",description:"",notes:"notes&comments"}}}
+    ,{"addTypedNode":{type:"Cluster",data:{name:"Wealth",description:"",notes:"notes&comments"}}}
+    ,{"addTypedNode":{type:"Cluster",data:{name:"Africa",description:"",notes:"notes&comments"}}}
     ,{"addTypedRelationship":{fromType:"Cluster",fromName:"Treasury",rel:"HAS_PARTS",toType:"Cluster",toName:"ALM",rdata:""}}
-    ,{"addTypedNode":{type:"Cluster",data:{name:"FTP",decription:"Funds Transfer Pricing",notes:"notes&comments"}}}
+    ,{"addTypedNode":{type:"Cluster",data:{name:"FTP",description:"Funds Transfer Pricing",notes:"notes&comments"}}}
     ,{"addTypedRelationship":{fromType:"Cluster",fromName:"Treasury",rel:"HAS_PARTS",toType:"Cluster",toName:"FTP",rdata:""}}
-    ,{"addTypedNode":{type:"Cluster",data:{name:"Liquidity",decription:"Liquididity Management",notes:"notes&comments"}}}
+    ,{"addTypedNode":{type:"Cluster",data:{name:"Liquidity",description:"Liquididity Management",notes:"notes&comments"}}}
     ,{"addTypedRelationship":{fromType:"Cluster",fromName:"Treasury",rel:"HAS_PARTS",toType:"Cluster",toName:"Liquidity",rdata:""}}
-    ,{"addTypedNode":{type:"Cluster",data:{name:"Funding",decription:"Funding",notes:"notes&comments"}}}
+    ,{"addTypedNode":{type:"Cluster",data:{name:"Funding",description:"Funding",notes:"notes&comments"}}}
     ,{"addTypedRelationship":{fromType:"Cluster",fromName:"Treasury",rel:"HAS_PARTS",toType:"Cluster",toName:"Funding",rdata:""}}
-    ,{"addTypedNode":{type:"Cluster",data:{name:"Capital",decription:"Capital Management",notes:"notes&comments"}}}
+    ,{"addTypedNode":{type:"Cluster",data:{name:"Capital",description:"Capital Management",notes:"notes&comments"}}}
     ,{"addTypedRelationship":{fromType:"Cluster",fromName:"Treasury",rel:"HAS_PARTS",toType:"Cluster",toName:"Capital",rdata:""}}
-    ,{"addTypedNode":{type:"Application",data:{name:"LDB",decription:""}}}
-    ,{"addTypedNode":{type:"Application",data:{name:"LCR",decription:""}}}
-    ,{"addTypedNode":{type:"Application",data:{name:"QRM",decription:""}}}
-    ,{"addTypedNode":{type:"Application",data:{name:"Poseidon",decription:""}}}
-    ,{"addTypedNode":{type:"Application",data:{name:"UKBA",decription:""}}}
-    ,{"addTypedNode":{type:"Business Process",data:{name:"Hedge the structural IRR",decription:""}}}
-    ,{"addTypedRelationship":{fromType:"Business Process",fromName:"Hedge the structural IRR",rel:"RELIES_ON",toType:"Application",toName:"QRM",rdata:""}}
-    ,{"addTypedRelationship":{fromType:"Cluster",fromName:"ALM",rel:"IS_ACCOUNTABLE_FOR",toType:"Business Process",toName:"Hedge the structural IRR",rdata:""}}
+    ,{"addTypedNode":{type:"Application",data:{name:"LDB",description:""}}}
+    ,{"addTypedNode":{type:"Application",data:{name:"LCR",description:""}}}
+    ,{"addTypedNode":{type:"Application",data:{name:"QRM",description:""}}}
+    ,{"addTypedNode":{type:"Application",data:{name:"Poseidon",description:""}}}
+    ,{"addTypedNode":{type:"Application",data:{name:"UKBA",description:""}}}
+    ,{"addTypedNode":{type:"BusinessProcess",data:{name:"Hedge the structural IRR",description:""}}}
+    ,{"addTypedRelationship":{fromType:"BusinessProcess",fromName:"Hedge the structural IRR",rel:"RELIES_ON",toType:"Application",toName:"QRM",rdata:""}}
+    ,{"addTypedRelationship":{fromType:"Cluster",fromName:"ALM",rel:"IS_ACCOUNTABLE_FOR",toType:"BusinessProcess",toName:"Hedge the structural IRR",rdata:""}}
     ,{"addTypedRelationship":{fromType:"Cluster",fromName:"UK Retail Bank",rel:"USES",toType:"Application",toName:"UKBA",rdata:""}}
     ,{"addTypedRelationship":{fromType:"Cluster",fromName:"Wealth",rel:"USES",toType:"Application",toName:"UKBA",rdata:""}}
 ]
@@ -34,13 +34,13 @@ n.addTypedRelationship("Cluster","ALM","IS_ACCOUNTABLE_FOR","Business Process","
 n.addTypedRelationship("Business Process","Hedge the strucutral IRR","RELIES_ON","Application","QRM")
 
 
-n.addTypedNode("Cluster",{name:"Treasury",decription:"",notes:"notes&comments"})
-n.addTypedNode("Cluster",{name:"ALM",decription:"Asset and Liablility Management",notes:"notes&comments"})
-n.addTypedNode("Cluster",{name:"FLM",decription:"Funding and Liquiditiy Management",notes:"notes&comments"})
-n.addTypedNode("Cluster",{name:"UK Retail Bank",decription:"",notes:"notes&comments"})
-n.addTypedNode("Cluster",{name:"Investment Bank",decription:"",notes:"notes&comments"})
-n.addTypedNode("Cluster",{name:"Western Europe",decription:"",notes:"notes&comments"})
-n.addTypedNode("Cluster",{name:"Africa",decription:"",notes:"notes&comments"})
+n.addTypedNode("Cluster",{name:"Treasury",description:"",notes:"notes&comments"})
+n.addTypedNode("Cluster",{name:"ALM",description:"Asset and Liablility Management",notes:"notes&comments"})
+n.addTypedNode("Cluster",{name:"FLM",description:"Funding and Liquiditiy Management",notes:"notes&comments"})
+n.addTypedNode("Cluster",{name:"UK Retail Bank",description:"",notes:"notes&comments"})
+n.addTypedNode("Cluster",{name:"Investment Bank",description:"",notes:"notes&comments"})
+n.addTypedNode("Cluster",{name:"Western Europe",description:"",notes:"notes&comments"})
+n.addTypedNode("Cluster",{name:"Africa",description:"",notes:"notes&comments"})
 
 n.addTypedNode("Application",{name:"LDB"})
 n.addTypedNode("Application",{name:"LCR"})
